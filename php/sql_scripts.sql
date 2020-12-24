eb59f242056daae6a48de9c1a5ada0ba8ed2a122
jlJJ <jl548339@dal.ca>
2020-12-23 22:51:43 -0400

    mv to heroku

--- php/sql_scripts.sql
+++
@@ -1,9 +0,0 @@
-use jiali;
-create table account(
-	id int(4) NOT NULL AUTO_INCREMENT,
-	first_name varchar(255) NOT NULL,
-    last_name varchar(255) NOT NULL,
-	`password` varchar(255) NOT NULL,
-    email varchar(255) NOT NULL ,
-    primary key (id)
-)
